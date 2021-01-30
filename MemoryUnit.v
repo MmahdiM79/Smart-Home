3c20a9131f7cb68244acbbb500522c2e70a94788
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-01-30 11:19:20 +0330

    MemoryUnit module added

---
+++ MemoryUnit.v
@@ -0,0 +1,46 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Memory Unit
+---  Description: Module6:
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module MemoryUnit (
+	input         arst , // async  reset
+	input         clk  , // clock  posedge
+	input         wren , // write  enabledata
+	input  [34:0] din  , // input  data
+	output [34:0] dout   // output data
+);
+    
+    reg [34:0] mem;
+    
+    always @(posedge clk or posedge arst) 
+    begin
+
+        if (arst)
+            mem = 35'b00000000000000000000000000000000000;
+        else
+            if (wren)
+                mem = din;
+        
+    end
+
+    assign dout = mem;
+
+endmodule
